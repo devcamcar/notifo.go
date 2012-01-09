a59752da24e4e3f0519a5802a727201d530db2cc
Dustin Sallings <dustin@spy.net>
2012-01-09 00:53:18 -0800

    Updates for go version weekly.2011-12-22 11120

--- notifo.go
+++ notifo.go
@@ -1,70 +1,77 @@
 package notifo
 
 import (
-    "http"
-    "os"
-    "restful"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 type NotifoApiClient struct {
-    endpoint    string;
-    apiusername string;
-    apisecret   string;
+	endpoint    string
+	apiusername string
+	apisecret   string
 }
 
-func New(apiusername, apisecret string) (*NotifoApiClient) {
-    return &NotifoApiClient {
-        endpoint:       "https://api.notifo.com/v1/",
-        apiusername:    apiusername,
-        apisecret:      apisecret,
-    }
+func New(apiusername, apisecret string) *NotifoApiClient {
+	return &NotifoApiClient{
+		endpoint:    "https://api.notifo.com/v1/",
+		apiusername: apiusername,
+		apisecret:   apisecret,
+	}
 }
 
 func (api *NotifoApiClient) SetEndpoint(endpoint string) {
-    api.endpoint = endpoint;
+	api.endpoint = endpoint
 }
 
-func (api *NotifoApiClient) SubscribeUser(username string) (*http.Response, os.Error) {
-    data := make(map[string]string);
-    data["username"] = username;
-    
-    return api.submitRequest("subscribe_user", "POST", data);
+func (api *NotifoApiClient) SubscribeUser(username string) (*http.Response, error) {
+	data := make(map[string]string)
+	data["username"] = username
+
+	return api.submitRequest("subscribe_user", "POST", data)
 }
 
 func (api *NotifoApiClient) SendNotification(to string, msg string, label string,
-        title string, uri string) (*http.Response, os.Error) {    
-    data := make(map[string]string);
+	title string, uri string) (*http.Response, error) {
+	data := make(map[string]string)
 
-    // TODO(devcamcar): Fail if to is blank.
-    data["to"] = to;
-    
-    // TODO(devcamcar): Fail if msg is blank.
-    data["msg"] = msg;
-    if len(label) > 0 {
-        data["label"] = label;
-    }
-    if len(title) > 0 {
-        data["title"] = title;
-    }
-    if len(uri) > 0 {
-        data["uri"] = uri;
-    }
-    
-    return api.submitRequest("send_notification", "POST", data);
+	// TODO(devcamcar): Fail if to is blank.
+	data["to"] = to
+
+	// TODO(devcamcar): Fail if msg is blank.
+	data["msg"] = msg
+	if len(label) > 0 {
+		data["label"] = label
+	}
+	if len(title) > 0 {
+		data["title"] = title
+	}
+	if len(uri) > 0 {
+		data["uri"] = uri
+	}
+
+	return api.submitRequest("send_notification", "POST", data)
 }
 
-func (api *NotifoApiClient) submitRequest(action, method string, params map[string]string) (*http.Response, os.Error ) {
-    var resp *http.Response
-    var err   os.Error
+func (api *NotifoApiClient) submitRequest(action, method string, params map[string]string) (*http.Response, error) {
+	vals := url.Values{}
+	for k, v := range params {
+		vals.Add(k, v)
+	}
 
-    client := &restful.RestClient {
-        Endpoint: api.endpoint,
-        UserInfo: api.apiusername + ":" + api.apisecret,
-    }
-    
-    if resp, err = client.SubmitRequest(action, method, nil, params); err != nil {
-       return nil, err
-    } 
-    
-    return resp, nil
+	client := http.Client{}
+
+	req, err := http.NewRequest(method, strings.Join([]string{api.endpoint, action}, "/"),
+		strings.NewReader(vals.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(api.apiusername, api.apisecret)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
